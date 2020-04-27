d32dc37ca9a45f56b6eb3a11f2e44ac71c195775
Aaron Cooper <cooper@pop-os.localdomain>
2020-04-27 11:02:49 -0700

    reset to basic scaffold template from AppBrewery

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,8 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.lightGreenAccent,
-          title: Center(
-            child: Text(
-              "I'm Poor",
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.blueGrey,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-          ),
-        ),
-        body: Center(
-          child: Image(
-            image: AssetImage('images/flower.png'),
-          ),
-        ),
+        backgroundColor: Colors.teal,
+        body: Container(),
       ),
     ),
   );
