d46aa706f8f774143818856c0aea4abc1f5e78d4
Aaron Cooper <cooper@pop-os.localdomain>
2020-04-27 20:52:57 -0700

    completed column and row challenge

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,39 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: double.infinity,
+                  color: Colors.red,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 100,
+                  height: double.infinity,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          )),
     ),
   );
 }
